84b0926e3fe0aed2044ed6043fc4531a962e6c4d
quitterie lanta <quitterielanta@gmail.com>
2023-05-26 12:09:22 +0100

    corrections watch list1

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,13 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
   def index
     @lists = List.all
-    @movies = Movie.all
-
   end
 
   def show
-    @list = List.find_by(id: params[:id])
-    @movie = Movie.find_by(id: params[:id])
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
   end
 
   def new
@@ -15,16 +15,26 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(params.require(:list).permit(:name, :image_url, :comment))
+    @list = List.new(list_params)
     if @list.save
-      redirect_to lists_path(@list)
+      redirect_to list_path(@list)
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
-  def new_bookmark
-    @list = List.find_by(id: params[:id])
-    @bookmark = Bookmark.new
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name, :photo)
   end
 end
